9cbb3851649d36f3ac9135401343317df824c9c2
Angiefiona <angellamumoh@gmail.com>
2023-02-03 07:56:13 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    var fullName: String = "Angela Mumo"
+    println(fullName)
+    var age: Byte = 20
+    println(age)
+    var phoneNumber: String = "0768214929"
+    println(phoneNumber)
+    var weightKg: Int = 40
+    println(weightKg)
+    var citizenKenya: Boolean = true
+    println(citizenKenya)
+
+}
